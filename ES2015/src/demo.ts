87758ba1bf0999e8d8b0a5190d4c643cb2998a44
pattabhia <shivaram2004@gmail.com>
2021-08-25 08:23:21 +0530

    adding for of loops example

--- ES2015/src/demo.ts
+++ ES2015/src/demo.ts
@@ -20,11 +20,45 @@ employee.greet();
 
 let displayColors = function (message: any, ...colors: any) {
     console.log(message);
-    for (let i in colors) {
-        console.log(colors[i])
+    for (let i of colors) {
+        console.log(i)
     }
 }
 
 let colorArray = ['red', 'yellow', 'blue'];
 displayColors('colors now..', ...colorArray)
+
+class Person {
+
+    constructor(name: any) {
+        console.log(`parent constructor ${name}`);
+    }
+
+    getID = () => {
+        console.log('getting parent getID');
+
+        return 10;
+    };
+}
+
+class Employee extends Person {
+
+    constructor(name: any) {
+
+        super(name);
+
+        console.log(`child constructor ${name}`);
+
+    }
+
+    getID = () => {
+
+        console.log('getting child getID');
+
+        return 50;
+    };
+}
+
+let member = new Employee('ram');
+console.log(member.getID());
 
